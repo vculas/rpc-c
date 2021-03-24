8ae5847c19afacde1d450a78480c77ce0b3a0d49
vculas <vinaculasvc@gmail.com>
2021-03-23 21:25:53 -0700

    Create rpc.c

---
+++ rpc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define ROCK 0
+#define PAPER 1
+#define SCISSORS 2
+
+
+int main()
+{
+   printf("VINA'S ROCK, PAPER, SCISSORS GAME\n\n");
+
+   printf("Choose Your Weapon: ");
+
+   return 0;
+}
